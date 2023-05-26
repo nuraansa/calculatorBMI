adbe2fb126788faa5d60457e3c2645178b57d5f4
nuraansa <nuraansa1@gmail.com>
2023-05-26 12:12:48 +0200

    Update

---
+++ script/code.js
@@ -0,0 +1,26 @@
+//Variables
+let weightInput = document.querySelector(".weight-input");
+let heightInput = document.querySelector(".height-input");
+let calculateButton = document.querySelector(".calculate");
+let result = document.querySelector(".result");
+let statement = document.querySelector(".result-statement");
+let BMI, height, weight;
+
+//Calculation
+calculateButton.addEventListener("click", ()=>{
+    
+    height = heightInput.value;
+    weight = weightInput.value;
+    BMI = weight/(height**2); 
+    result.innerText = BMI;
+
+    if(BMI < 18.5){
+        statement.innerText = "You are underweight!";    
+    }else if((BMI > 18.5) && (BMI <= 24.9)){
+        statement.innerText = "You are normal weight!";
+    }else if((BMI >= 25) && (BMI <= 29.9 )){
+        statement.innerText = "You are overweight!";
+    }else{
+        statement.innerText = "You are obese!";
+    }
+});
